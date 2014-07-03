b0451f68296457a4052529ab214105477524eef6
Brian J Brennan <brianloveswords@gmail.com>
2014-07-03 02:54:06 -0400

    Add some tests!

--- 008: Largest product in a series/largest-product2.rs
+++ 008: Largest product in a series/largest-product2.rs
@@ -17,7 +17,7 @@ impl PartialOrd for Result {
 
 impl PartialEq for Result {
     fn eq(&self, other: &Result) -> bool {
-        self.product == other.product
+        self.product == other.product && self.digits == other.digits
     }
 }
 
@@ -33,7 +33,7 @@ fn main() {
         c.to_digit(10).unwrap()
     }).collect();
 
-    let mut max = Result { product: 0, digits: vec!(0) };
+    let mut max = Result { product: 0, digits: vec![0] };
     for i in range(0, digits.len()) {
         match product_of_next(&digits, i, 13) {
             None => break,
@@ -47,10 +47,11 @@ fn main() {
 }
 
 fn product_of_next(vec: &Vec<uint>, start: uint, amount: uint) -> Option<Result> {
-    let limit = start + amount;
+    let end = start + amount;
+
     let mut digits = Vec::new();
 
-    for i in range(start, limit) {
+    for i in range(start, end) {
         let maybe_digit = safe_get(vec, i);
 
         if maybe_digit.is_none() {
@@ -62,7 +63,7 @@ fn product_of_next(vec: &Vec<uint>, start: uint, amount: uint) -> Option<Result>
         digits.push(digit);
     }
 
-    let product = digits.iter().fold(1, |a, &x| a * (x as u64));
+    let product = digits.iter().fold(1, |a, x| a * (*x as u64));
     Some(Result { product: product, digits: digits })
 }
 
@@ -71,4 +72,22 @@ fn safe_get<'a, T>(vec: &'a Vec<T>, idx: uint) -> Option<&'a T> {
         return None
     }
     Some(vec.get(idx))
+}
+
+mod tests {
+    #[test]
+    fn safe_get_test() {
+        let v = vec![1u,2,3];
+        assert_eq!(::safe_get(&v, 900), None);
+        assert_eq!(::safe_get(&v, 1), Some(&2));
+        assert_eq!(::safe_get(&v, -1), None);
+    }
+
+    #[test]
+    fn product_of_next_test() {
+        let v = vec![1u, 2, 3, 4, 5, 6];
+        assert_eq!(::product_of_next(&v, 0, 1), Some(::Result{ product: 1, digits: vec![1]}));
+        assert_eq!(::product_of_next(&v, 0, 4), Some(::Result{ product: 1*2*3*4, digits: vec![1, 2, 3, 4]}));
+        assert_eq!(::product_of_next(&v, 0, 100), None);
+    }
 }
